bac94a9c071bca261ed9eadb3e265c37be103306
epson agunda <epsonagunda182@gmail.com>
2024-05-07 07:49:13 -0700

    regular expressions

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,3 +2,4 @@
 # The regular expression must match School
 #create a Ruby script that accepts one argument and pass it
 # to a regular expression matching method
+uts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -2,3 +2,5 @@
 # regular expression must be exactly matching a string that
 # starts with h ends with n
 #create a Ruby script that accepts one argument and pass it to a
+#regular expression matching method
+puts ARGV[0].scan(/h.n/).join
